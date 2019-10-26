c2cc73b5a70743d4a29de844db062c9cba7b0987
Jonathan Combs <JofCombs@gmail.com>
2019-10-26 10:18:18 -0700

    signup now saves the NETID for later broadcasting

--- app/src/main/java/com/example/test/signup.java
+++ app/src/main/java/com/example/test/signup.java
@@ -2,7 +2,9 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.EditText;
 
 public class signup extends AppCompatActivity {
@@ -17,11 +19,16 @@ public class signup extends AppCompatActivity {
     public void go_back(android.view.View view) {
         EditText net_id = this.findViewById(R.id.net_idEditText);
         String id = net_id.getText().toString();
-        //java.lang.System.out.println("bluh");
+        //check for valid input:
         if(id.length() != 6) {
             net_id.setError("Please enter your full NetID");
         }else{
-            //save the text?
+            //if valid, save the text:
+            SharedPreferences app_data = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor data = app_data.edit();
+            data.putString("NETID", id);
+            data.commit();
+            //and go back:
             finish();
         }
     }
