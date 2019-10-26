5d66c79df3d363f0f8163aae792f6622ec6c79b6
Jonathan Combs <JofCombs@gmail.com>
2019-10-26 15:40:44 -0700

    maybe makes SI end of bluetooth work

--- app/src/main/java/com/example/test/AttendenceScanner.java
+++ app/src/main/java/com/example/test/AttendenceScanner.java
@@ -3,42 +3,48 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.util.UUID;
+
 public class AttendenceScanner extends AppCompatActivity {
+    byte[] uuidgen = {50, 42, 122};
+    UUID uuid =  UUID.nameUUIDFromBytes(uuidgen);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendence_scanner);
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+
         BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
         if (bluetooth == null) {
             // Device doesn't support Bluetooth
-            //android:id="@+id/scanning"
             TextView label = this.findViewById(R.id.scanning);
             label.setText("Bluetooth not working!");
             Button exit = this.findViewById(R.id.button);
             exit.setText("Return");
-            //delete progress bar?
-            //ProgressBar bar = this.findViewById(R.id.progressBar);
         }else{
-
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 600);
+            startActivity(intent);
+            try{
+                bluetooth.listenUsingRfcommWithServiceRecord("Glorious Leader", uuid);
+            }catch(java.io.IOException e){
+                finish();
+            }
         }
-
     }
 
     @Override
-    protected void onPause() {
+    protected void onDestroy() {
+        Intent intent = new Intent(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        startActivity(intent);
 
-        super.onPause();
+        super.onDestroy();
     }
 
     public void end(android.view.View v){
