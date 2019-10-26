e3f694bf1c66b82860b3bfbe62055edc95569a87
Jonathan Combs <JofCombs@gmail.com>
2019-10-26 13:47:49 -0700

    make bluetooth work maybe eventually

--- app/src/main/java/com/example/test/AttendenceScanner.java
+++ app/src/main/java/com/example/test/AttendenceScanner.java
@@ -2,7 +2,11 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class AttendenceScanner extends AppCompatActivity {
 
@@ -10,6 +14,31 @@ public class AttendenceScanner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendence_scanner);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
+        if (bluetooth == null) {
+            // Device doesn't support Bluetooth
+            //android:id="@+id/scanning"
+            TextView label = this.findViewById(R.id.scanning);
+            label.setText("Bluetooth not working!");
+            Button exit = this.findViewById(R.id.button);
+            exit.setText("Return");
+            //delete progress bar?
+            //ProgressBar bar = this.findViewById(R.id.progressBar);
+        }else{
+
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
     }
 
     public void end(android.view.View v){
