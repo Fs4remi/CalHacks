1b9d463e91142da1003fecbe559d956b15c7946c
Fatem <f.s4remi@gmail.com>
2019-10-26 08:38:03 -0700

    Set up SET NET ID button

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void sign_up_button(android.view.View view){
