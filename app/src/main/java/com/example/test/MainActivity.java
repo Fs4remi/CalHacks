c0f277b82a625af6926fe5dd27c175209873f8a2
Jonathan Combs <JofCombs@gmail.com>
2019-10-26 12:38:24 -0700

    make startup switch

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences app_data = PreferenceManager.getDefaultSharedPreferences(this);
+        String id = app_data.getString("NETID", "");
+        String email = app_data.getString("EMAIL", "");
+        Intent intent;
+        if(!email.isEmpty())
+            intent = new Intent(this, MainAdmin.class);
+        else if(!id.isEmpty())
+            intent = new Intent(this, MainStudent.class);
+        else
+            intent = new Intent(this, initialSetUp.class);
+        startActivity(intent);
+    }
+}
